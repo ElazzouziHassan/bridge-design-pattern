2e069400a8017fc2a6c5d79a78d3182104fdcee7
wizardy <wizardhassan99@gmail.com>
2025-01-08 16:49:53 +0100

    commit

--- src/bridge/remote/AdvancedRemoteControl.java
+++ src/bridge/remote/AdvancedRemoteControl.java
@@ -11,3 +11,12 @@ public class AdvancedRemoteControl extends RemoteControl {
       device.setVolume(volume);
   }
 }
+
+/*
+ * Rôle : Représente une télécommande avancée qui peut allumer/éteindre l'appareil et régler son volume.
+ * 
+ * Explication :
+ * Hérite de la classe abstraite RemoteControl.
+ * Implémente la méthode setVolume(int volume) pour régler le volume de l'appareil.
+ * Utilise également la méthode togglePower() de la classe parente pour allumer/éteindre l'appareil.
+ */
