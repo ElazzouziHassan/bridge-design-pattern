69e3fe7c10912085ca9d26f81e303c5f624856b4
wizardy <wizardhassan99@gmail.com>
2025-01-08 16:49:59 +0100

    commit

--- src/bridge/remote/RemoteControl.java
+++ src/bridge/remote/RemoteControl.java
@@ -1,5 +1,4 @@
 package bridge.remote;
-
 import bridge.IDevice;
 
 public abstract class RemoteControl {
@@ -19,3 +18,14 @@ public abstract class RemoteControl {
 
     public abstract void setVolume(int volume); // Méthode abstraite pour régler le volume
 }
+/*
+ * Rôle : Sert de pont entre les télécommandes (abstraction) et les appareils (implémentation).
+ * 
+ * Explication :
+ * La classe abstraite RemoteControl représente une télécommande générique.
+ * Elle contient une référence à un appareil (Device), permettant à la télécommande de contrôler différents
+ * appareils (comme une TV ou une Radio).
+ * La méthode togglePower() permet d'allumer ou d'éteindre l'appareil de manière alternée.
+ * La méthode setVolume(int volume) est déclarée comme abstraite, car le comportement de réglage du volume peut
+ * différer entre les télécommandes.
+ */
