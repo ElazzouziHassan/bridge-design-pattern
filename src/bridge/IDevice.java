83221b977589b1cf6c63769f6edf26613554fb50
wizardy <wizardhassan99@gmail.com>
2025-01-08 16:49:37 +0100

    commit

--- src/bridge/IDevice.java
+++ src/bridge/IDevice.java
@@ -1,8 +1,18 @@
 package bridge;
 
 public interface IDevice {
-  void powerOn();
-  void powerOff();
-  void setVolume(int volume);
-  boolean isPoweredOn();
+  void powerOn(); // Allumer l'appareil
+  void powerOff(); // Éteindre l'appareil
+  void setVolume(int volume); // Régler le volume
+  boolean isPoweredOn(); // Vérifier si l'appareil est allumé ou éteint
 }
+
+/*
+ * @Explication:
+ * powerOn() et powerOff() : Définissent les actions pour allumer/éteindre l'appareil.
+ * setVolume(int volume) : Permet de régler le volume de l'appareil.
+ * isPoweredOn() : Renvoie true si l'appareil est allumé, sinon false.
+ * -------------------------------------------------------------------------------------------
+ * => Cette interface permet de garantir que tous les appareils qui l'implémentent (TV, Radio) 
+ * auront un comportement cohérent.
+ */
